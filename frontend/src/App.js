514682dd7627e1aa9646cdad2c85d1e67752bd7e
Khaled <khaled.aldaqqa@gmail.com>
2022-06-02 13:03:35 -0700

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+import HomeScreens from './screens/HomeScreens'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreens} exact />
+        </Container>
+      </main>
+     <Footer />
+    </Router>
+  );
+}
+
+export default App;
