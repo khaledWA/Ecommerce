fd408c371aea2f385975837a05ef7e30d78d8bb4
khaledWA <khaled.aldaqqa@gmail.com>
2023-09-23 18:49:29 -0400

    Full project upload

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
+import { addToCart } from '../actions/cartActions'
+
+function CartScreen({ match, location, history }) {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const { cartItems } = cart
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
+
+
+    const removeFromCartHandler = (id) => {
+        
+    }
+
+    const checkoutHandler = () => {
+        history.push('/login?redirect=shipping')
+    }
+
+    return (
+        <Row>
+            <Col md={8}>
+                <h1>Shopping Cart</h1>
+                {cartItems.length === 0 ? (
+                    <Message variant='info'>
+                        Your car is empty <Link to='/'>Go Back</Link>
+                    </Message>
+                ) : (
+                    <ListGroup variant='flush'>
+
+                    </ListGroup>
+                )}
+            </Col>
+
+            <Col md={4}>
+            </Col>
+        </Row>
+    )
+
+    }
+
+export default CartScreen
